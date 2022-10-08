93ced8cd325d59135b8c4c71b53e34d22bf4d0c5
msandeepk <90695071+mssandeepkamath@users.noreply.github.com>
2022-10-09 02:24:08 +0530

    Create welcome.kt

---
+++ welcome.kt
@@ -0,0 +1,22 @@
+fun backend()
+{
+ println("Studybear backend is built using Kotlin")
+}
+
+fun frontend()
+{
+ println("Studybear frontend is designed using XML"); 
+}
+
+fun deploy()
+{
+  println("Studybear is live in Playstore")
+}
+
+fun main()
+{
+  frontend()
+  backend()
+  deploy()
+  print("Thank you")
+}
